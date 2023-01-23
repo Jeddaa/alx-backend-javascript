384468aa01527c37105980509fff64408e576d0d
jeddaa <fathiato@gmail.com>
2023-01-23 22:32:39 +0100

    task 2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -3,7 +3,6 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ["SOMA", "Union Square"];
 
-  const self = this;
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;

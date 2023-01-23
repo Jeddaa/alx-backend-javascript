e9f8ad5e7cdac26688e432c306e4d49adcadb3ea
jeddaa <fathiato@gmail.com>
2023-01-24 00:26:19 +0100

    task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,10 @@
+/** @format */
+
+export default function appendToEachArrayValue(array, appendString) {
+  for (let idx in array) {
+    let value = array[idx];
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}

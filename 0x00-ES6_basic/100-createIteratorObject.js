871d045b42fee76f5a988f6e57b668f452204ce0
jeddaa <fathiato@gmail.com>
2023-01-24 01:38:24 +0100

    task 13

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,9 @@
+/** @format */
+
+export default function createIteratorObject(report) {
+  const newList = [];
+  for (const x of Object.values(report.allEmployees)) {
+    newList.push(...x);
+  }
+  return newList;
+}
